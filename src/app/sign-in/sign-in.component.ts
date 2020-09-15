6b2b708e9dd75b23bd8e654ec290809419edd480
StationedInTheField <ask@stationedinthefield.com>
2020-09-14 22:50:12 -0700

    flex storage

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -189,6 +189,9 @@ async openFileLoaded(event){
       }
       else{
         this.ui.showSnack('Swarm Error =(','Start Over', {duration:8000});
+        // if(String(error).indexOf("Cannot read property 'channelKeyChain' of null") > -1){
+        //   window.location.reload();
+        // }
         console.log(error);
         await this.ui.delay(5000);
       //  window.location.reload();
